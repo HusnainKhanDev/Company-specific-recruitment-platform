a7aa613f3bc771e9dbc06c9c2995eb9433ef5da7
HusnainKhanDev <husnainkhan.swe@gmail.com>
2025-09-17 21:32:21 +0500

    Edits Docker Files

--- BackEnd/src/App.ts
+++ BackEnd/src/App.ts
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use('/Uploads', express.static('Uploads'));
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:3000"],
     credentials: true
 }))
 
